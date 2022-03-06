08ed93c704725212baa30c1d3f7fdb648ee64485
alfovo <afvolpert@gmail.com>
2022-03-06 00:05:23 -0500

    adding tic tac toe module from react tutorial

---
+++ src/features/tic-tac-toe/components/Board.js
@@ -0,0 +1,38 @@
+import Square from "./Square";
+import React from "react";
+
+export default class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        key={i}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  renderBoardRow(index) {
+    const columns = [];
+    let squareIndex = index;
+    for (let i = 0; i < 3; i++) {
+      columns.push(this.renderSquare(squareIndex));
+      squareIndex++;
+    }
+    return (
+      <div key={index} className="board-row">
+        {columns}
+      </div>
+    );
+  }
+
+  render() {
+    const rows = [];
+    let squareIndex = 0;
+    for (let i = 0; i < 3; i++) {
+      rows.push(this.renderBoardRow(squareIndex));
+      squareIndex = squareIndex + 3;
+    }
+    return <div>{rows}</div>;
+  }
+}
